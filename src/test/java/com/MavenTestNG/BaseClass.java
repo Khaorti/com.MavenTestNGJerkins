f62dd7065c54ca715373e4ae5d83a6ae20641aec
safe.johns@gmail.com <safe.johns@gmail.com>
2018-12-29 09:01:28 -0800

    initial

--- src/test/java/com/MavenTestNG/BaseClass.java
+++ src/test/java/com/MavenTestNG/BaseClass.java
@@ -2,13 +2,17 @@ package com.MavenTestNG;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class BaseClass {
 	
+	@Test
 	public void setup () {
 		System.setProperty("webdriver.chome.drivers", "Driver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver ();
-		driver.get("");
+		driver.get("https://www.google.com/");
+		
+		
 		
 	}
 	
